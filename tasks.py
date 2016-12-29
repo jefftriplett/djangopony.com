0c0f0196136dd59b6a821294e101985502bea431
Jeff Triplett <jeff.triplett@gmail.com>
2016-12-29 13:53:24 -0600

    Updates invoke to work in the present tense

--- tasks.py
+++ tasks.py
@@ -2,11 +2,11 @@ from invoke import run, task
 
 
 @task
-def build():
+def build(ctx):
     run('s3cmd --acl-public put index.html s3://www.djangopony.com/index.html')
     run('s3cmd --acl-public sync media/ s3://static.djangopony.com/')
 
 
 @task
-def runserver():
+def runserver(ctx):
     run('python -m SimpleHTTPServer')
